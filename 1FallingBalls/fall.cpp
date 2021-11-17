9c2ad18559a42b14b77014f9ef93b1e3dcf9c7fb
lutcraft <739750084@qq.com>
2021-11-17 23:25:40 +0800

    fall ok!

--- 1FallingBalls/fall.cpp
+++ 1FallingBalls/fall.cpp
@@ -4,8 +4,55 @@
 int main(int argc, char const* argv[])
 {
     initgraph(600, 600, NULL);
-    fillcircle(300, 300, 100);
-    _getch();
+    float g = 0.5;
+    float vy = 0;
+    float vx = 20;
+    float y = 300;
+    float x = 300;
+    float zuli = 0.9;
+    while (true)
+    {
+        vy += g;
+        y += vy;
+        x += vx;
+        fillcircle(x, y, 20);
+        Sleep(10);
+        cleardevice();
+        if (y >= 580)
+        {
+            vy *= -zuli;
+        }
+        if (y <= 20)
+        {
+            vy *= -zuli;
+        }
+        if (x >= 580) 
+        {
+            vx *= -zuli;
+        }
+        if (x <= 20)
+        {
+            vx *= -zuli;
+        }
+        if (y > 580)
+        {
+            y = 580;
+        }
+        if (y < 20)
+        {
+            y = 20 + (20 - y);
+        }
+        if (x > 580)
+        {
+            x = 580 - (x - 580);
+        }
+        if (x < 20)
+        {
+            x = 20 + (x - 20);
+        }
+    }
+
+
     closegraph();
     return 0;
 }
