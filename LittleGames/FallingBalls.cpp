2b7ee78da61021dd8850060183c590f91cddcbd2
lutcraft <739750084@qq.com>
2021-11-17 22:52:34 +0800

    添加项目文件。

---
+++ LittleGames/FallingBalls.cpp
@@ -0,0 +1,13 @@
+#include <graphics.h>
+#include <conio.h>
+#include <stdio.h>
+int main(int argc, char const* argv[])
+{
+    initgraph(600, 600, NULL);
+    fillcircle(300, 300, 100);
+    _getch();
+    closegraph();
+    return 0;
+}
+
+
---
+++ LittleGames/Hello.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main(int argc, char const* argv[])
+{
+    printf("Hello World\n");
+    _getch();
+    return 0;
+}
